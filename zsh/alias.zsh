6d931bb2d664b98afd6cb47a256516033a97cd4f
David James <david@jamesmail.net>
2019-05-01 18:50:23 -0500

    Update zsh settings

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,9 +1,9 @@
 alias cl='clear'
-
-alias py='python'
-alias vim='nvim'
+alias py='python3'
 alias jv='java -version'
-
+alias vim='nvim'
+alias psa='source env/bin/activate'
+alias ctags="$(brew --prefix)/bin/ctags"
 alias zrc='vim $ZDOTDIR/.zshrc'
 alias zal='vim $ZDOTDIR/alias.zsh'
 
--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,10 +1,22 @@
-bindkey -v
-
 #tab completion
 autoload -U compinit
 compinit
+setopt correctall
 
 #directory navigation
 setopt autocd autopushd
 
+#history
+setopt hist_ignore_all_dups
+setopt hist_ignore_space
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+setopt notify
+
+# prompt
+export PS1="[%*] (%C) > "
+
+# other
 export EDITOR=/usr/local/bin/nvim
+bindkey -v
